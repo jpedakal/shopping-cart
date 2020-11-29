fba9afb79243ce914dcb0f42317db174db47ce06
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-29 12:55:49 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
-const assert = require('assert');
+const { assert, expect, should } = require('chai');
 
-describe('Array', function(){
-    describe('#indexOf()', function(){
-        it('It should return value -1 if value is not present', function(){
-            assert.equal([1,2,3].indexOf(4), -1);
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('It should return value -1 if value is not present', function () {
+            assert.equal([1, 2, 3].indexOf(2), -1);
         });
     });
 });

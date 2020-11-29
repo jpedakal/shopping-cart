973a1c362b0e648d52fef72667ce441b32fa7b78
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-29 14:09:54 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -4,35 +4,35 @@ const languages= {software_lan: ['C', 'Java', 'Javascript']}
 
 // assert test cases
 
-describe('#typeOf', function () {
-    it('type of value should be string', function () {
-        assert.typeOf(foo, 'string');   // Without optional message
-    });
-});
+// describe('#typeOf', function () {
+//     it('type of value should be string', function () {
+//         assert.typeOf(foo, 'string');   // Without optional message
+//     });
+// });
 
-describe('#typeOf', function () {
-    it('type of value should be string', function () {
-        assert.typeOf(foo, 'string', 'foo is a string');   // With optional message
-    });
-});
+// describe('#typeOf', function () {
+//     it('type of value should be string', function () {
+//         assert.typeOf(foo, 'string', 'foo is a string');   // With optional message
+//     });
+// });
 
-describe('#equal', function () {
-    it('it shoud give true if both strings are equal', function () {
-        assert.equal(foo, 'boo');
-    });
-});
+// describe('#equal', function () {
+//     it('it shoud give true if both strings are equal', function () {
+//         assert.equal(foo, 'boo');
+//     });
+// });
 
-describe('#length', function () {
-    it('length of array', function () {
-        assert.lengthOf(foo, 3);
-    });
-});
+// describe('#length', function () {
+//     it('length of array', function () {
+//         assert.lengthOf(foo, 3);
+//     });
+// });
 
-describe('#length', function () {
-    it('length of array', function () {
-        assert.lengthOf(languages.software_lan, 3);
-    });
-});
+// describe('#length', function () {
+//     it('length of array', function () {
+//         assert.lengthOf(languages.software_lan, 3);
+//     });
+// });
 
 // expect test cases
 

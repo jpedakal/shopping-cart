e8073c12cb8ea921918b4408cd8c38bfe6edd171
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-29 12:46:35 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,1 +1,9 @@
 const assert = require('assert');
+
+describe('Array', function(){
+    describe('#indexOf()', function(){
+        it('It should return value -1 if value is not present', function(){
+            assert.equal([1,2,3].indexOf(4), -1);
+        });
+    });
+});

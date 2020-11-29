55300ad85f7aa5d51a53b0b14cbb8b99f0884cbd
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-29 13:09:56 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,9 +1,27 @@
 const { assert, expect, should, use } = require('chai');
+const foo = 'boo';
 
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('It should return value -1 if value is not present', function () {
-            assert.equal([1, 2, 3].indexOf(2), -1);
-        });
+
+describe('#typeOf', function () {
+    it('type of value should be string', function () {
+        assert.typeOf(foo, 'string');   // Without optional message
+    });
+});
+
+describe('#typeOf', function () {
+    it('type of value should be string', function () {
+        assert.typeOf(foo, 'string', 'foo is a string');   // With optional message
+    });
+});
+
+describe('#equal', function () {
+    it('it shoud give true if both strings are equal', function () {
+        assert.equal(foo, 'boo');
+    });
+});
+
+describe('#length', function () {
+    it('length of array', function () {
+        assert.lengthOf(foo, 3);
     });
 });
